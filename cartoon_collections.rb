b1040499eaf74cb949114b798723403d6e15d3fa
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-09-29 02:34:49 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ end
 
 def long_planeteer_calls(callz)# code an argument here
   # Your code here
-  callz.collect do |call|
+  callz.map do |call|
     if call.size > 4
       return true
     else
