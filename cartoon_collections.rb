8ece15231e8ff731377a24957b8a67f135c6a2e5
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-09-29 01:50:06 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
 def roll_call_dwarves(dwarves)# code an argument here
-
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+    index +=1
+  end
     # Your code here
 end
 
