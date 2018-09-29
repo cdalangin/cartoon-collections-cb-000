2b0f37bd099447f332a38b985e8db41f7e431821
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-09-29 02:10:25 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,12 @@ def long_planeteer_calls(callz)# code an argument here
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheedz)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+    if cheedz.include(cheese_types)
+      cheedz.collect do |types|
+        puts "#{types}"
+      end
+    end
 end
