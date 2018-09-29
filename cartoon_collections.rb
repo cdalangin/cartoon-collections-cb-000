75302a54ed50987635b8d388c362f8b8cdbaf461
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-09-29 01:38:49 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)# code an argument here
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{dwarf}"
+    index +=1
+  end
+    # Your code here
 end
 
 def summon_captain_planet# code an argument here
